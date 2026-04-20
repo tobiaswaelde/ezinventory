8dbb66609bc54465cadd8d1e3b8d959c8e2638d4
Tobias Wälde <tobias.waelde@gmail.com>
2026-04-20 19:39:38 +0200

    feat(api): add prisma schema and database module foundation

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { HealthModule } from './modules/health/health.module.js';
 import { ItemsModule } from './modules/items/items.module.js';
+import { PrismaModule } from './prisma/prisma.module.js';
 
 @Module({
-  imports: [HealthModule, ItemsModule]
+  imports: [PrismaModule, HealthModule, ItemsModule]
 })
 export class AppModule {}
