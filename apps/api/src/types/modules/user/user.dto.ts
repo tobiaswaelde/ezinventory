8c1cfc0f9e41a0722cdaefc6701fe11cb210f6d2
Tobias Wälde <tobias.waelde@gmail.com>
2026-04-28 03:22:40 +0200

    fix user dto

--- apps/api/src/types/modules/user/user.dto.ts
+++ apps/api/src/types/modules/user/user.dto.ts
@@ -1,9 +1,11 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Exclude, Expose } from 'class-transformer';
 import { ApiPropertyCreatedAt } from '~/decorators/properties/api-property-created-at.decorator';
 import { ApiPropertyId } from '~/decorators/properties/api-property-id.decorator';
 import { ApiPropertyUpdatedAt } from '~/decorators/properties/api-property-updated-at.decorator';
 import { UserPayload } from '~/types/modules/user';
+import { UserPreferencesDTO } from '~/types/modules/user-preferences/user-preferences.dto';
+import { UserProfileDTO } from '~/types/modules/user-profile/user-profile.dto';
 
 export class UserDTO {
   @Expose()
@@ -40,13 +42,13 @@ export class UserDTO {
   @ApiProperty({ description: 'Token used for websocket authentication' })
   websocketToken: string;
 
-  // @Expose()
-  // @ApiPropertyOptional({ type: () => UserProfileDTO })
-  // profile?: UserProfileDTO;
+  @Expose()
+  @ApiPropertyOptional({ type: () => UserProfileDTO })
+  profile?: UserProfileDTO;
 
-  // @Expose()
-  // @ApiPropertyOptional({ type: () => UserPreferencesDTO })
-  // preferences?: UserPreferencesDTO;
+  @Expose()
+  @ApiPropertyOptional({ type: () => UserPreferencesDTO })
+  preferences?: UserPreferencesDTO;
 
   constructor(partial: Partial<UserDTO>) {
     Object.assign(this, partial);
@@ -57,8 +59,8 @@ export class UserDTO {
 
     const dto = new UserDTO({
       ...model,
-      // preferences: await UserPreferencesDTO.fromModel(model.preferences, ability),
-      // profile: await UserProfileDTO.fromModel(model.profile, ability),
+      preferences: await UserPreferencesDTO.fromModel(model.preferences),
+      profile: await UserProfileDTO.fromModel(model.profile),
     });
 
     // return CaslUtil.filterKeys(dto, RoleSubject.USER, ability);
